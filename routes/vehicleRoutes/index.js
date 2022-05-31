2864546ae1669847e7f84190d1ac00a855ba885f
DAMMIKA940 <dammikapadmasiri123@gmail.com>
2022-05-31 08:24:32 +0530

    vehicle upload

--- routes/vehicleRoutes/index.js
+++ routes/vehicleRoutes/index.js
@@ -1,9 +1,9 @@
 const routes = require('express').Router();
-
+const storage = require("../../lib/multerConfig")
 const VehicleController = require("../../controllers/vehicle.controller");
 
 
-routes.post("/addVehicle", VehicleController.addVehicle);
+routes.post("/addVehicle",storage.single('image'), VehicleController.addVehicle);
 routes.get("/getAllVehicles", VehicleController.getAllVehicles);
 
 
