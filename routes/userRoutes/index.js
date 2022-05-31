e23ef81d660af426ffbfac36370705a228ae90ef
UltraSL <64199585+UltraSL@users.noreply.github.com>
2022-05-31 20:03:35 +0530

    sadf

--- routes/userRoutes/index.js
+++ routes/userRoutes/index.js
@@ -24,9 +24,9 @@ routes.get("/profile/:id", UserController.getUser);
 routes.get("/getAllUsers", UserController.getAllUsers);
 routes.delete("/deleteUser/:id", UserController.deleteUser);
 routes.get("/getMngByDepart/:department", UserController.getAllSupervisorsByDepartment);
-routes.put("/resetPassword/:email", UserController.resetPassword);
-routes.put("/changePassword/:email", UserController.changePassword);
-
+routes.get("/resetPassword/:email", UserController.resetPassword);
+routes.get("/changePassword/:email", UserController.changePassword);
+ 
 
 //resetPassword
 module.exports = routes;
